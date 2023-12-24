77fd5f19a8cc3349372d453511f229ae960f5265
Viraj76 <virajg.7680@gmail.com>
2023-12-24 18:13:07 +0530

    adapter for products

--- app/src/main/java/com/example/adminblinkitclone/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/adminblinkitclone/fragments/HomeFragment.kt
@@ -7,15 +7,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.adminblinkitclone.Constants
 
 import com.example.adminblinkitclone.R
+import com.example.adminblinkitclone.adapter.AdapterProduct
 import com.example.adminblinkitclone.adapter.CategoriesAdapter
 import com.example.adminblinkitclone.databinding.FragmentHomeBinding
 import com.example.adminblinkitclone.model.Categories
+import com.example.adminblinkitclone.viewmodels.AdminViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 
 class HomeFragment : Fragment() {
+    val viewModel : AdminViewModel by viewModels()
     private lateinit var binding  : FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +32,20 @@ class HomeFragment : Fragment() {
         setStatusBarColor()
 
         setCategories()
+
+        getAllTheProducts()
         return binding.root
+    }
+
+    private fun getAllTheProducts() {
+        lifecycleScope.launch {
+            viewModel.fetchAllTheProducts().collect{
+                val adapterProduct = AdapterProduct()
+                binding.rvProducts.adapter = adapterProduct
+                adapterProduct.differ.submitList(it)
+            }
+        }
+
     }
 
     private fun setCategories() {
--- app/src/main/java/com/example/adminblinkitclone/viewmodels/AdminViewModel.kt
+++ app/src/main/java/com/example/adminblinkitclone/viewmodels/AdminViewModel.kt
@@ -4,10 +4,16 @@ import android.net.Uri
 import androidx.lifecycle.ViewModel
 import com.example.adminblinkitclone.Utils
 import com.example.adminblinkitclone.model.Product
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.callbackFlow
 import java.util.UUID
 
 class AdminViewModel : ViewModel() {
@@ -63,5 +69,28 @@ class AdminViewModel : ViewModel() {
             }
     }
 
+
+    fun fetchAllTheProducts() : Flow<List<Product>> = callbackFlow {
+        val db = FirebaseDatabase.getInstance().getReference("Admins").child("AllProducts")
+
+        val eventListener = object  : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val products = ArrayList<Product>()
+                for (product in snapshot.children){
+                    val prod = product.getValue(Product::class.java)
+                    products.add(prod!!)
+                }
+                trySend(products)
+            }
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        db.addValueEventListener(eventListener)
+
+        awaitClose{db.removeEventListener(eventListener)
+        }
+    }
 
 }
